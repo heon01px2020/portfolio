1b509e1b06ad565faffd6b2af710dde08ef1c822
heon01px2020 <heon01px2020@gmail.com>
2024-06-07 09:23:10 +0800

    fixing deployment issues

--- src/components/blog_posts/lytnetv1/LYTNetv1.js
+++ src/components/blog_posts/lytnetv1/LYTNetv1.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+import React from 'react';
 import 'katex/dist/katex.min.css';
 // import { BlockMath, InlineMath } from 'react-katex';
 // import figure1 from './figure1.png';
