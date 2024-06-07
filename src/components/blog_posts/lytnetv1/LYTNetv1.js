3f8d748b386f18b7d5c64af160c24f0f37d629c5
heon01px2020 <heon01px2020@gmail.com>
2024-06-07 09:20:38 +0800

    fixing deployment issues

--- src/components/blog_posts/lytnetv1/LYTNetv1.js
+++ src/components/blog_posts/lytnetv1/LYTNetv1.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import 'katex/dist/katex.min.css';
-import { BlockMath, InlineMath } from 'react-katex';
-import figure1 from './figure1.png';
-import figure2 from './figure2.png';
-import figure3 from './figure3.png';
-import figure4 from './figure4.png';
-import figure5 from './figure5.png';
+// import { BlockMath, InlineMath } from 'react-katex';
+// import figure1 from './figure1.png';
+// import figure2 from './figure2.png';
+// import figure3 from './figure3.png';
+// import figure4 from './figure4.png';
+// import figure5 from './figure5.png';
 import BlogPostLayout from '../../blog_posts/BlogPostLayout';
 
 import { Helmet } from 'react-helmet';
@@ -297,38 +297,38 @@ const LYTNetv1 = () => {
     );
 };
 
-const LYTNet = () => {
-    const selectedCategories = ['Artificial Intelligence'];
-    const [htmlContent, setHtmlContent] = useState('');
+// const LYTNet = () => {
+//     const selectedCategories = ['Artificial Intelligence'];
+//     const [htmlContent, setHtmlContent] = useState('');
 
-    useEffect(() => {
-        fetch('/blog_posts/lytnetv1/lytnetv1.html')
-            .then(response => response.text())
-            .then(data => setHtmlContent(data))
-            .catch(error => console.error('Error fetching HTML:', error));
-    }, []);
+//     useEffect(() => {
+//         fetch('/blog_posts/lytnetv1/lytnetv1.html')
+//             .then(response => response.text())
+//             .then(data => setHtmlContent(data))
+//             .catch(error => console.error('Error fetching HTML:', error));
+//     }, []);
 
-    return (
-        <BlogPostLayout selectedCategories={selectedCategories}>
-            {/* <h1 className="text-4xl font-bold mb-4" style={{ color: '#183F7C' }}>
-                Proving Graph 3–Coloring with Go and Zero–Knowledge Proofs
-            </h1>
-            <div className="prose prose-lg">
-                <p>In this blog post, we will explore the concept of zero-knowledge proofs applied to graph 3-coloring using the Go programming language.</p>
-                <p>Here is an example of a LaTeX inline formula: <InlineMath math="a^2 + b^2 = c^2\sum_{i=1}^n i^2" />.</p>
-                <p>And here is a block formula:</p>
-                <BlockMath math="\int_0^\infty x^2 dx" />
-            </div> */}
-            <div className="content" dangerouslySetInnerHTML={{ __html: htmlContent }} />
-            {/* <h1 className="text-4xl font-bold mb-4" style={{ color: '#183F7C' }}>
-            LYTNet: A Convolutional Neural Network for Real-Time Pedestrian Traffic Lights and Zebra Crossing Recognition for the Visually Impaired
-            </h1>
-            <h1 className="text-2xl font-bold mb-4" style={{ color: '#183F7C' }}>
-            Samuel Yu, Heon Lee, John Kim
-            </h1> */}
-            {/* <div className="content"> */}
-        </BlogPostLayout>
-    );
-}
+//     return (
+//         <BlogPostLayout selectedCategories={selectedCategories}>
+//             {/* <h1 className="text-4xl font-bold mb-4" style={{ color: '#183F7C' }}>
+//                 Proving Graph 3–Coloring with Go and Zero–Knowledge Proofs
+//             </h1>
+//             <div className="prose prose-lg">
+//                 <p>In this blog post, we will explore the concept of zero-knowledge proofs applied to graph 3-coloring using the Go programming language.</p>
+//                 <p>Here is an example of a LaTeX inline formula: <InlineMath math="a^2 + b^2 = c^2\sum_{i=1}^n i^2" />.</p>
+//                 <p>And here is a block formula:</p>
+//                 <BlockMath math="\int_0^\infty x^2 dx" />
+//             </div> */}
+//             <div className="content" dangerouslySetInnerHTML={{ __html: htmlContent }} />
+//             {/* <h1 className="text-4xl font-bold mb-4" style={{ color: '#183F7C' }}>
+//             LYTNet: A Convolutional Neural Network for Real-Time Pedestrian Traffic Lights and Zebra Crossing Recognition for the Visually Impaired
+//             </h1>
+//             <h1 className="text-2xl font-bold mb-4" style={{ color: '#183F7C' }}>
+//             Samuel Yu, Heon Lee, John Kim
+//             </h1> */}
+//             {/* <div className="content"> */}
+//         </BlogPostLayout>
+//     );
+// }
 
 export default LYTNetv1;
