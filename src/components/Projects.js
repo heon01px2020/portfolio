f62adbd06796fd3ed24b7ebd33218974f34071c8
heon01px2020 <heon01px2020@gmail.com>
2025-02-16 17:03:15 -0500

    import katex

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import Layout from './Layout';
 import ReactMarkdown from 'react-markdown';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 import 'katex/dist/katex.min.css';
 
 // Importing images from assets directory
