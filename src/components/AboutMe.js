0903f140fc6a9a6f7ff88814ce932b05747d4ac8
heon01px2020 <heon01px2020@gmail.com>
2025-02-17 23:24:57 -0500

    update about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,37 +8,37 @@ const AboutMe = () => {
       <div className="p-8 max-w-3xl">
         <h1 className="text-4xl font-bold mb-4">About Me</h1>
         <p className="text-xl mb-4">
-        Hello! I am Heon, a rising junior at Brown University studying 
-        Computer Science and Mathematics. Originally from Seoul, Korea, 
-        I grew up in Shanghai, China, and am currently studying in Providence. 
+        Hello! I'm Heon, a junior at Brown University studying 
+        Computer Science and Mathematics. 
+        {/* Originally from Seoul, Korea, 
+        I grew up in Shanghai, China and am currently studying in Providence. 
         With a unique cultural perspective, I communicate effectively and 
         thrive in various environments, making me an excellent team player. 
-        Furthermore, I am adaptable and am always eager to learn new things.
+        Furthermore, I am adaptable and am always eager to learn new things. */}
         </p>
         <p className="text-xl mb-4">
-        My main interests lie in machine learning, operations research, 
-        and mathematical analysis, but I am also interested in algebra, 
-        probability, mathematical education, topology, quantitative finance, 
-        game theory, and theoretical computer science.
+        My main interests are in theoretical computer science. More 
+        specifically, I do machine learning and optimization. I also have 
+        a strong interest in related fields such as mathematical analysis, algebra, 
+        probability, and topology.
         </p>
         <p className="text-xl mb-4">
-        At Brown, I have been involved with the Outreach Committee under 
-        the Undergraduate Student Council and will be introducing Brown 
-        to incoming first-years as a Meiklejohn. I have also been a TA 
-        for Graph Theory (MATH1230) and Fairness in Automated Decision 
-        Making (CSCI1951z) and will be a TA for Design and Analysis of 
-        Algorithms (CSCI1570) this fall (2024). I am also researching the 
-        theoretical aspects of algorithms using iterated linear optimization 
-        and semidefinite programming with Professor Pedro Felzenszwalb.
+        At Brown, I've served on the Outreach Committee and am introducing Brown 
+        to incoming first-years as a Meiklejohn. I have also been a head TA 
+        for Fairness in Automated Decision Making (CSCI1951z), and a TA for 
+        Graph Theory (MATH1230) and Design and Analysis of Algorithms (CSCI1570). 
+        Furthermore, I research theoretical algorithms with Professor Pedro Felzenszwalb, 
+        focusing on iterated linear optimization and semidefinite programming.
         </p>
         <p className="text-xl mb-4">
-        My passion for tackling complex problems fuels my pursuit of computer 
+        My passion for tackling complex problems drives my pursuit of computer 
         science and mathematics. I love the challenge of diving into difficult 
         math problems, whether I find them online or through friends. Recently, 
-        I’ve been particularly drawn to the interplay between geometry and algebra, 
-        where their connection feels almost magical.
+        I'm fascinated by the interplay between geometry and algebra, a connection 
+        that's become even more exciting as I frequently find it important in 
+        my current research!
         </p>
-        <p className="text-xl mb-4">
+        {/* <p className="text-xl mb-4">
         This summer, I'm excited to participate in a math directed reading program 
         on Geometric Group Theory. This fascinating field explores the hidden 
         connection between abstract groups and geometric objects. By studying how 
@@ -47,8 +47,8 @@ const AboutMe = () => {
         is the ability to use the geometric properties of these objects to uncover 
         underlying algebraic truths about the groups themselves. It's like analyzing 
         a sculpture to understand the mathematical principles that govern its form!
-        </p>
-        <p className="text-xl mb-4">
+        </p> */}
+        {/* <p className="text-xl mb-4">
         Beyond theoretical pursuits, I love applying math and CS to 
         real-world challenges. For instance, my friends and I developed 
         and published a paper on LYTNet, a lightweight CNN architecture 
@@ -58,8 +58,8 @@ const AboutMe = () => {
         build an app for that. Additionally, recognizing the limitations of 
         dating apps that rely on collaborative filtering, my team and I 
         created a web platform that matches people based on personality traits.
-        </p>
-        <p className="text-xl mb-4">
+        </p> */}
+        {/* <p className="text-xl mb-4">
         I'm also socially conscious about the impact of technology, especially 
         AI, in current times. As a TA for the course Fairness in Automated 
         Decision Making, I've contributed to developing assignments that adopt 
@@ -69,7 +69,7 @@ const AboutMe = () => {
         probabilistic discriminative models—from the paper by Toshihiro 
         Kamishima et al. I've also helped design a new final project where 
         students audit an AI system modeling a real-world situation.
-        </p>
+        </p> */}
         <p className="text-xl">
           Feel free to connect via email (heon_lee at brown.edu).
         </p>
