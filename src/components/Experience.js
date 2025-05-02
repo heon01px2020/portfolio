ea0727e39f89b18a8ad333d9f945f5e753e5d543
heon01px2020 <heon01px2020@gmail.com>
2025-05-02 12:30:40 -0400

    add courses

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -22,9 +22,9 @@ const Experience = () => {
                 <h3 className="text-xl font-semibold">Brown University <span className="text-gray-600">Providence, RI</span></h3>
                 <p className="text-lg">Bachelor of Science in Computer Science and Mathematics</p>
                 <p className="text-gray-600">August 2022 to May 2026</p>
-                <p><strong>Algorithms/ML/DL Courses:</strong> Robust Algorithms for ML; Algorithmist's Toolkit; Design and Analysis of Algorithms; Fairness in Automated Decision Making; Deep Learning; Discrete Math</p>
-                <p><strong>Optimization/Stats Courses:</strong> Recent Applications of Probability and Statistics; Convex Analysis and Optimization Algorithms; Probability, Optimization, and Stochastic Calculus; Statistical Inference</p>
-                <p><strong>Math Courses:</strong> Grad Algebra; Analysis on Manifolds I/II; Graph Theory; Ordinary Differential Equations; Number Theory; Honors Linear Algebra; Honors Multivariable Calculus</p>
+                <p><strong>Algorithms/ML/DL Courses:</strong> Robust Algorithms for ML; Algorithmist's Toolkit; Advanced Topics in DL; Design and Analysis of Algorithms; Fairness in Automated Decision Making; Deep Learning; Discrete Math</p>
+                <p><strong>Optimization/Stats Courses:</strong> Recent Applications of Probability and Statistics; Convex Analysis and Optimization Algorithms; Advanced Probabilistic Methods in CS; Probability, Optimization, and Stochastic Calculus; Statistical Inference</p>
+                <p><strong>Math Courses:</strong> Grad Algebra I/II; Analysis on Manifolds I/II; Graph Theory; ODE/PDE; Number Theory; Honors Linear Algebra; Honors Multivariable Calculus</p>
                 <p><strong>Teaching Assistant:</strong></p>
                 <ul className="list-disc list-inside">
                   <li>Fairness in Automated Decision Making (HTA, Spring 2025; UTA, Spring 2024)</li>
